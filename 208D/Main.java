d8f4877f255fa135289f9f1b8a663eed4b2aa206
shivam24121 <Shivam050905@gmail.com>
2025-10-15 21:59:05 +0530

    Create Main.java

---
+++ 208D/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int a=Integer.parseInt(st.nextToken());
+		int b=Integer.parseInt(st.nextToken());
+		System.out.println((a*30)+(b*60));
+	}
+}
