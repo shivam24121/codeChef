76e22d056bbf8d070f14c74117cda66a5f3817ad
shivam24121 <Shivam050905@gmail.com>
2025-10-22 22:05:57 +0530

    Create Main.java

---
+++ 209D/Bitcoin Market/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int n=obj.nextInt();
+		System.out.println(n<=4?"YES":"NO");
+
+	}
+}
