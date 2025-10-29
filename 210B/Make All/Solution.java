75357d7528791f7029c23c1f6546342806dafd31
shivam24121 <Shivam050905@gmail.com>
2025-10-29 22:10:17 +0530

    Create Solution.java

---
+++ 210B/Make All/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    static int[] a={1,2,3,6,9,18,27,54,81,162,243,486,729,1458,2187,4374,6561
+                    ,13122,19683,39366};
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int n=obj.nextInt();
+		
+		for(int i=0;i<a.length;i++){
+		    System.out.println(a[i]+" ");
+		}
+
+	}
+}
